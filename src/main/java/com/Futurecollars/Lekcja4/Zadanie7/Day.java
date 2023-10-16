3e09a61b3bb293d7414bce48e4833c230e0d5196
Damian Kowalski <damkow3847@gmail.com>
2023-10-16 19:28:12 +0200

    Lekcja 4 Zadanie 7

---
+++ src/main/java/com/Futurecollars/Lekcja4/Zadanie7/Day.java
@@ -0,0 +1,11 @@
+package com.Futurecollars.Lekcja4.Zadanie7;
+
+    public enum Day {
+        Poniedzialek,
+        Wtorek,
+        Sroda,
+        Czwartek,
+        Piatek,
+        Sobota,
+        Niedziela
+    }
