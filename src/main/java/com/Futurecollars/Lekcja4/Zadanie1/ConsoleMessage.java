ddcf9d290e6387f3ea426c9ce96d062bc943ad52
Damian Kowalski <damkow3847@gmail.com>
2023-10-16 19:22:06 +0200

    Lekcja 4 Zadanie 1

---
+++ src/main/java/com/Futurecollars/Lekcja4/Zadanie1/ConsoleMessage.java
@@ -0,0 +1,12 @@
+package com.Futurecollars.Lekcja4.Zadanie1;
+
+public class ConsoleMessage extends Message{
+
+    String getConsoleMessage= "This is Console Message";
+    @Override
+    public String getMessage()
+    {
+        return getConsoleMessage;
+    }
+
+}
