d7020b9bea92833efb39bf2dcdebfa549593d099
Damian Kowalski <damkow3847@gmail.com>
2023-10-31 21:34:21 +0100

    Lekcja 6 Zadanie 6

---
+++ src/main/java/com/Futurecollars/Lekcja6/Zadanie8/IllegalLengthException.java
@@ -0,0 +1,7 @@
+package com.Futurecollars.Lekcja6.Zadanie8;
+
+public class IllegalLengthException extends RuntimeException {
+    public IllegalLengthException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/Futurecollars/Lekcja6/Zadanie8/Validation.java
@@ -0,0 +1,8 @@
+package com.Futurecollars.Lekcja6.Zadanie8;
+
+public class Validation {
+    public static void main(String[] args) {
+        PeselValidation pesel = new PeselValidation();
+        System.out.println(pesel.pesel("k1111111111"));
+    }
+}
