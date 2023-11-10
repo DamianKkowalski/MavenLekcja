67cffb538a12f70a5627cea3af7cfd684e5f6585
Damian Kowalski <damkow3847@gmail.com>
2023-11-10 13:10:24 +0100

    Lekcja 8 Zadanie 4

---
+++ src/main/java/com/Futurecollars/Lekcja9/Zadanie1/Basic.java
@@ -0,0 +1,13 @@
+package com.Futurecollars.Lekcja9.Zadanie1;
+
+public class Basic {
+
+        String key;
+        String value;
+
+        public Basic(String key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
