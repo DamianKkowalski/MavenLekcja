cd76ae57da07d7426c09f624c4db55774d754d0a
Damian Kowalski <damkow3847@gmail.com>
2024-01-12 16:18:44 +0100

    lekcja 7 zadanie 1 poprawione formatowanie

--- src/main/java/com/Futurecollars/Lekcja7/Zadanie1/SumAndDivide.java
+++ src/main/java/com/Futurecollars/Lekcja7/Zadanie1/SumAndDivide.java
@@ -5,22 +5,18 @@ public class SumAndDivide {
     int digit;
     int sumNumbers;
 
-    public boolean divideBy2(int number)
-    {
-        if (number%2==0)
-        {
+    public boolean divideBy2(int number) {
+        if (number % 2 == 0) {
             return true;
         }
         return false;
     }
-    public int sum(int numbers)
-    {
-        while(numbers>0)
 
-        {
-            digit=numbers%10;
-            sumNumbers=sumNumbers+digit;
-            numbers=numbers/10;
+    public int sum(int numbers) {
+        while (numbers > 0) {
+            digit = numbers % 10;
+            sumNumbers = sumNumbers + digit;
+            numbers = numbers / 10;
         }
         return sumNumbers;
     }
--- src/test/java/com/Futurecollars/Lekcja7/Zadanie1/SumAndDivideTest.java
+++ src/test/java/com/Futurecollars/Lekcja7/Zadanie1/SumAndDivideTest.java
@@ -8,17 +8,18 @@ import org.junit.jupiter.params.provider.ValueSource;
 public class SumAndDivideTest {
     @ParameterizedTest
     @ValueSource(ints = {2, 3, 4, 512, 1444, -38})
-    void shouldVerifyIsNumberDividedByTwo(int number){
+    void shouldVerifyIsNumberDividedByTwo(int number) {
 
-        SumAndDivide sad=new SumAndDivide();
+        SumAndDivide sad = new SumAndDivide();
         Assertions.assertTrue(sad.divideBy2(number));
-}
-    @ParameterizedTest
-    @CsvSource(value = {"324:9","-324:-9","420:6","0:0"}, delimiter=':')
-        void shouldVerifyIsNumberSum ( int input, int expected){
+    }
 
-            SumAndDivide sad = new SumAndDivide();
-            Assertions.assertEquals(expected,sad.sum(input));
-        }
+    @ParameterizedTest
+    @CsvSource(value = {"324:9", "-324:-9", "420:6", "0:0"}, delimiter = ':')
+    void shouldVerifyIsNumberSum(int input, int expected) {
+
+        SumAndDivide sad = new SumAndDivide();
+        Assertions.assertEquals(expected, sad.sum(input));
+    }
 
 }
