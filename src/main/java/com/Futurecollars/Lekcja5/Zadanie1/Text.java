ab5487b1f3364b4341675808db4c7ef8719c7bd4
Damian Kowalski <damkow3847@gmail.com>
2024-01-13 09:34:17 +0100

    lekcja 5 zadanie 1 dostosowano sie do uwag, wykonano refactoring, stworzony pomocniczne metody :) pozdrawiam

--- src/main/java/com/Futurecollars/Lekcja5/Zadanie1/Text.java
+++ src/main/java/com/Futurecollars/Lekcja5/Zadanie1/Text.java
@@ -1,44 +1,35 @@
 package com.Futurecollars.Lekcja5.Zadanie1;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-
 public class Text {
-    public static void main(String[] args) {
-        String Text = "naaana";
-        String regex = ".*[a-zA-Z].*";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcherText = pattern.matcher(Text); //finding letters
-        Boolean textMatches = matcherText.matches(); //return
-
-        if (textMatches == true) {
-            System.out.println("Tekst istnieje i ma dlugosc znakow " + Text.length());
-        }
-
-
-        //checking for palindrome
-
-        String textToLowerCase = Text.toLowerCase();
-        int length = Text.length();
-        int forward = 0;
-        int backward = length - 1;
-        boolean Logic = true;
-
-        while (backward > forward && Logic) {
-            char forwardChar = textToLowerCase.charAt(forward++);
-            char backwardChar = textToLowerCase.charAt(backward--);
-            if (forwardChar != backwardChar) {
-                Logic = false;
-            }
-        }
-        if (Logic == true)
-            System.out.println("To jest palindrom " + Text);
-
-        else
-            System.out.println("To nie jest palindrom " + Text);
+    public static boolean isEmpty(String text) {
+        return text.matches(".*[a-zA-Z].*");
     }
 
+    public static int countCharacters(String text) {
+        return text.length();
+    }
 
+    public static boolean isPalindrome(String text) {
+        String cleanText = text.replaceAll("[^a-zA-Z]", "").toLowerCase();
+        StringBuilder reversedText = new StringBuilder(cleanText).reverse();
+        return cleanText.equals(reversedText.toString());
+    }
+
+    public static void main(String[] args) {
+        String text = "kajak";
+        if (isEmpty(text)) {
+            System.out.println("The text contains characters");
+
+            if (isPalindrome(text)) {
+                System.out.println("The text is a palindrome");
+            } else {
+                System.out.println("The text is not a palindrome");
+            }
+            int textLength = countCharacters(text);
+            System.out.println("The length of the text is: " + textLength);
+        } else {
+            System.out.println("The text is empty");
+        }
+    }
 
 }
