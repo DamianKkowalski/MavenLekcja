6c7eca373b8792980de91646c0a93a6fc0981f75
Damian Kowalski <damkow3847@gmail.com>
2024-01-13 09:49:32 +0100

    lekcja 5 zadanie 2 dostosowano sie do uwag, wykonano refactoring :) pozdrawiam

--- src/main/java/com/Futurecollars/Lekcja5/Zadanie2/FizzBuzz.java
+++ src/main/java/com/Futurecollars/Lekcja5/Zadanie2/FizzBuzz.java
@@ -1,27 +1,21 @@
 package com.Futurecollars.Lekcja5.Zadanie2;
 
 public class FizzBuzz {
-    public static void main(String[] args) {
-
-
-    int Number;
-
-    for(Number=1; Number<=100; Number++)
-    {
-        if (Number % 15==0)
-        {
-            System.out.println("FizzBuzz");
+    public static void playFizzBuzz(int endValue) {
+        for (int number = 1; number <= endValue; number++) {
+            if (number % 15 == 0) {
+                System.out.println("FizzBuzz");
+            }
+            if (number % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (number % 5 == 0) {
+                System.out.println("Buzz");
+            } else System.out.println(number);
         }
-        if (Number % 3==0)
-        {
-            System.out.println("Fizz");
-        }
-        else if (Number % 5==0)
-        {
-            System.out.println("Buzz");
-        }
-        else
-        System.out.println(Number);
     }
-}
+
+    public static void main(String[] args) {
+        int endValue = 100;
+        playFizzBuzz(endValue);
+    }
 }
