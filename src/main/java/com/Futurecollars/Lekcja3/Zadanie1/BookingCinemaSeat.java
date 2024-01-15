62a9f02e45ed73501a664759df3c2b5a8f5f12d0
Damian Kowalski <damkow3847@gmail.com>
2024-01-15 14:10:49 +0100

    lekcja 3 zadanie 1 poprawiono o uwagi

--- src/main/java/com/Futurecollars/Lekcja3/Zadanie1/BookingCinemaSeat.java
+++ src/main/java/com/Futurecollars/Lekcja3/Zadanie1/BookingCinemaSeat.java
@@ -1,21 +1,11 @@
 package com.Futurecollars.Lekcja3.Zadanie1;
 
-public class  BookingCinemaSeat {
+public class BookingCinemaSeat {
 
-    public static void main(String[] args)
-    {
-        int r = 10;
-        int seat = 12;
-        int NotOccupied=0;
-        int Occupied=1;
-
-        Cinema cinema = new Cinema();
-        cinema.row=r;
-        cinema.seat=seat;
-        String occupied=cinema.EmptyOrOccupied[Occupied];
-        System.out.println("Miejsce "+cinema.seat+" w rzedzie "+cinema.row+" jest "+occupied+".");
-
-
-
+    public static void main(String[] args) {
+        Cinema cinemaSeat1 = new Cinema(4, 5);
+        System.out.println(cinemaSeat1);
+        cinemaSeat1.occupySeat();
+        System.out.println(cinemaSeat1);
     }
 }
--- src/main/java/com/Futurecollars/Lekcja3/Zadanie1/Cinema.java
+++ src/main/java/com/Futurecollars/Lekcja3/Zadanie1/Cinema.java
@@ -1,10 +1,32 @@
 package com.Futurecollars.Lekcja3.Zadanie1;
 
-class Cinema
-{
+class Cinema {
 
-    int row;
-    int seat;
-    String[] EmptyOrOccupied={"wolne","zajete"};
 
+    private final int row;
+    private final int seat;
+
+    private SeatStatus seatStatus;
+
+    public Cinema(int row, int seat) {
+        this.row = row;
+        this.seat = seat;
+        this.seatStatus = SeatStatus.FREE;
+    }
+    public void occupySeat() {
+        seatStatus = SeatStatus.OCCUPIED;
+    }
+
+    public void freeSeat() {
+        seatStatus = SeatStatus.FREE;
+    }
+
+    @Override
+    public String toString() {
+        return "Cinema{" +
+                "row=" + row +
+                ", seat=" + seat +
+                ", seatStatus=" + seatStatus +
+                '}';
+    }
 }
